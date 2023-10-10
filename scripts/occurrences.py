df51f5b4abea1700314c81c2d75951ff90ae7f86
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2023-10-10 16:00:02 +0100

    Refactored the species.py script

--- scripts/occurrences.py
+++ scripts/occurrences.py
@@ -1,16 +1,21 @@
 import pandas as pd
 
-DATASET_OCCURRENCES = '../data/datasets/occurrence.txt'
-DATASET_VERBATIM = '../data/datasets/verbatim.txt'
+from constants import *
 
-CSV_OCCURRENCES = '../data/processed/occurrences_data.csv'
+DATASET_OCCURRENCES = '../data/datasets/sample_occurrence.txt'
+DATASET_VERBATIM = '../data/datasets/sample_verbatim.txt'
 
+CSV_OCCURRENCES = '../data/processed/sample_occurrences_data.csv'
+
+print('Executing occurrences.py')
 occurrences_df = pd.read_csv(DATASET_OCCURRENCES, sep='\t', low_memory=False)
+print(f'\t{CHAR_ARROW} Loaded occurrences.txt successfully')
+
 verbatim_df = pd.read_csv(DATASET_VERBATIM, sep='\t', low_memory=False)
+print(f'\t{CHAR_ARROW} Loaded verbatim.txt successfully')
 
 occurrences_columns = set(occurrences_df.columns)
 verbatim_columns = set(verbatim_df.columns)
-
 
 verbatim_only_columns = verbatim_columns - occurrences_columns
 
@@ -18,7 +23,7 @@ for column in verbatim_only_columns:
     occurrences_df[column] = verbatim_df[column]
 
 
-interestingColumns = [
+interesting_columns = [
     'species',
     'individualCount',
     'organismQuantity',
@@ -42,6 +47,7 @@ interestingColumns = [
     'elevation'
 ]
 
-occurrences_df = occurrences_df[interestingColumns]
+occurrences_df = occurrences_df[interesting_columns]
 
 occurrences_df.to_csv(CSV_OCCURRENCES)
+print(f'\t{CHAR_ARROW} Written a CSV with occurrences data successfully')
